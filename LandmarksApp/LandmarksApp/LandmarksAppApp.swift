3d2bfa56bf8a983aa766cfe67bac2f84f6fe643c
Anatoly Gurbanov <agurbanov@65apps.com>
2021-09-01 23:35:13 +0300

    Create Xcode project

---
+++ LandmarksApp/LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Anatoly Gurbanov on 01.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
