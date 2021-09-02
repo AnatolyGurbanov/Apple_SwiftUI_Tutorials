aca1b5c154bdb0fbd7e45a40de245411d76a0dce
Anatoly Gurbanov <agurbanov@65apps.com>
2021-09-03 00:59:03 +0300

    Added new models

---
+++ LandmarksApp/LandmarksApp/Entries/Coordinates.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinates.swift
+//  LandmarksApp
+//
+//  Created by Anatoly Gurbanov on 03.09.2021.
+//
+
+import Foundation
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
--- LandmarksApp/LandmarksApp/Entries/Landmark.swift
+++ LandmarksApp/LandmarksApp/Entries/Landmark.swift
@@ -5,7 +5,9 @@
 //  Created by Anatoly Gurbanov on 03.09.2021.
 //
 
+import CoreLocation
 import Foundation
+import SwiftUI
 
 struct Landmark: Hashable, Codable {
     var id: Int
@@ -13,4 +15,17 @@ struct Landmark: Hashable, Codable {
     var park: String
     var state: String
     var description: String
+
+    private var coordinates: Coordinates
+    private var imageName: String
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    var image: Image {
+        Image(imageName)
+    }
 }
