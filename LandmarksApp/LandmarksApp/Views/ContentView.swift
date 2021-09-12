a31e9333411ca80a5188da7f0f19b0255379a1f7
Anatoly Gurbanov <tomgurbanov@gmail.com>
2021-09-13 01:37:32 +0300

    Create screen with landmark list and detail

--- LandmarksApp/LandmarksApp/Views/ContentView.swift
+++ LandmarksApp/LandmarksApp/Views/ContentView.swift
@@ -9,40 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-                .ignoresSafeArea(edges: .top)
-
-            CircleImageView()
-                .offset(y: -180)
-                .padding(.bottom, -180)
-
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                    .fontWeight(.medium)
-
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-
-                Divider()
-
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
---
+++ LandmarksApp/LandmarksApp/Views/LandmarkList.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkList.swift
+//  LandmarksApp
+//
+//  Created by Anatoly Gurbanov on 13.09.2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
