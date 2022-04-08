592c85c0f72059f7140953c675bf8377a9684b83
Anatoly Gurbanov <tomgurbanov@gmail.com>
2022-04-09 00:32:56 +0600

    Create favorite button

--- LandmarksApp/LandmarksApp/Views/LandmarkDetail.swift
+++ LandmarksApp/LandmarksApp/Views/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
 
     var body: some View {
         ScrollView {
@@ -20,9 +25,11 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
 
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
-                    .foregroundColor(.primary)
+				HStack {
+					Text(landmark.name)
+						.font(.title)
+					FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+				}
 
                 HStack {
                     Text(landmark.park)
@@ -48,9 +55,10 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
-    static var landmarks = ModelData().landmarks
-
-    static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
-    }
+	static let modelData = ModelData()
+	
+	static var previews: some View {
+		LandmarkDetail(landmark: modelData.landmarks[0])
+			.environmentObject(modelData)
+	}
 }
